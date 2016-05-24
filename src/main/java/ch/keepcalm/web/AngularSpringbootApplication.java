c6cf80366118240c1df9e2fddc8b6db5cea03a52
Marcel Widmer <marzelwidmer@gmail.com>
2016-05-24 23:20:56 +0200

    add springboot maven setup

---
+++ src/main/java/ch/keepcalm/web/AngularSpringbootApplication.java
@@ -0,0 +1,12 @@
+package ch.keepcalm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootApplication.class, args);
+	}
+}
