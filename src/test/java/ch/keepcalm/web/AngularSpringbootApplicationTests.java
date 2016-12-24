e30375412c01d702df1601cadb07fe99eb89cefe
Marcel Widmer <marzelwidmer@gmail.com>
2016-12-24 23:55:23 +0100

    change test classes

--- src/test/java/ch/keepcalm/web/AngularSpringbootApplicationTests.java
+++ src/test/java/ch/keepcalm/web/AngularSpringbootApplicationTests.java
@@ -3,12 +3,14 @@ package ch.keepcalm.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AngularSpringbootApplicationTests {
 
     @Test
