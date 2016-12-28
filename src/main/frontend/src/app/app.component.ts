e26e2c3af66f24daf5cf6d8c35f262d005bbb423
Marcel Widmer <marzelwidmer@gmail.com>
2016-12-28 08:11:07 +0100

    work on app to add some todo's

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoDataService} from "./todo/todo-data.service";
+import {Todo} from "./todo/todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  // We first define a newTodo property and assign a
+  // new Todo() when the component class is instantiated.
+  // This is the same Todo instance specified in the two-way binding expression
+  // of [(ngModel)] in our view:
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  // addTodo(todo) {
+  //   this.todoDataService.addTodo(todo);
+  //   this.newTodo = new Todo();
+  // }
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
